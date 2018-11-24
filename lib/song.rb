ead1d4567ec5e69a6ae0b48f8201194efb06ee93
Fer Hernandez <fh.1111@gmail.com>
2018-11-24 23:41:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
   def self.find_or_create_by_name(songName)
     foundSong = self.find_by_name(songName)
     puts "#{foundSong}"
-    if ( foundSong == nil)
+    if ( foundSong == "")
       self.create_by_name(songName)
     else
       return nil
