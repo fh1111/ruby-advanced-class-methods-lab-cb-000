c1f036011d6b6552c6c2a385e8f6fbb517658b67
Fer Hernandez <fh.1111@gmail.com>
2018-11-24 23:40:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,42 @@ class Song
     self.class.all << self
   end
 
+  def self.create()
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(songName)
+    song = self.new
+    song.name =  songName
+    @@all << song
+    song
+  end
+
+  def self.create_by_name(songName)
+    song = self.new
+    song.name =  songName
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(songName)
+    @@all.find{|person| person.name == songName}
+  end
+
+  def self.find_or_create_by_name(songName)
+    foundSong = self.find_by_name(songName)
+    puts "#{foundSong}"
+    if ( foundSong == nil)
+      self.create_by_name(songName)
+    else
+      return nil
+    end
+
+  end
+
 end
+
+song_1 = Song.find_or_create_by_name("Sometimes")
+song_2 = Song.find_or_create_by_name("Sometimes")
